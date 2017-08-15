5c4ee349e52d9e80a79f6d1933a4f47ab8948f1d
Christopher Mohr <iismo01@u-003-ncmu03.(none)>
2017-08-15 11:28:43 +0200

    Final changes and bug fixes

--- runWorkflow.py
+++ runWorkflow.py
@@ -68,7 +68,7 @@ for mzml in mzmlFiles:
     identifer_for_file = idPath.split('/')[-1]
 
     idresult_fdr = os.path.join(result_path, identifer_for_file.replace('.idXML', '_merged_fdr.idXML'))
-    falseDiscovery = 'FalseDiscoveryRate -in {f} -out {o} -threads 20 -algorithm:add_decoy_peptides -algorithm:use_all_hits'.format(f=idresult,o=idresult_fdr)
+    falseDiscovery = 'FalseDiscoveryRate -in {f} -out {o} -threads 20 -algorithm:add_decoy_peptides -algorithm:use_all_hits'.format(f=idPath,o=idresult_fdr)
     subprocess.call(falseDiscovery.split(),stderr=logfile, stdout=logfile)
 
     ### extract Percolator Features with PSMFeatureExtractor on Mathias TopPerc branch
@@ -85,11 +85,10 @@ for mzml in mzmlFiles:
     idresult_filtered = os.path.join(result_path, '{}_merged_perc_fdr_filtered.idXML'.format(identifer_for_file))
     idFilter = 'IDFilter  -in {f} -out {o} -score:pep {m} -remove_decoys -threads 20'.format(f=idresult_fdr_psm, o=idresult_filtered, m=fdr)
     subprocess.call(idFilter.split(),stderr=logfile, stdout=logfile)
-    #TODO? also I'd suggest to filter dynamically on portal display
 
     ### map percolator refined and FDR filtered ids onto features
-    mergeresult = os.path.join(result_path, mzmlPath.replace('mzML', 'featureXML').split('/')[-1])
-    IDMapper = 'FeatureFinderIdentification -in {f} -id {i} -threads 20 -out {o}'.format(f=mzmlPath,i=idresult_filtered,o=mergeresult)
+    mergeresult = os.path.join(result_path, mzml.replace('mzML', 'featureXML').split('/')[-1])
+    IDMapper = 'FeatureFinderIdentification -in {f} -id {i} -threads 20 -out {o}'.format(f=mzml,i=idresult_filtered,o=mergeresult)
     subprocess.call(IDMapper.split(),stderr=logfile, stdout=logfile)
 
     ########################################################################################
@@ -108,10 +107,7 @@ for mzml in mzmlFiles:
             header = r.strip().split('\t')[1:] + opr[i - 1].strip().split('\t')[1:]
         if r.startswith('PEPTIDE'):
             if not opr[i-1].startswith('PEPTIDE'):
-                if r.strip().split('\t')[-1][-5:]=='HUMAN':
-                    df.append(r.strip().split('\t')[1:] + opr[i - 1].strip().split('\t')[1:])
-                else:
-                    df.append(r.strip().split('\t')[1:] + ['-'] + opr[i - 1].strip().split('\t')[1:])
+                df.append(r.strip().split('\t')[1:] + opr[i - 1].strip().split('\t')[1:])
 
     df=pd.DataFrame(df)
     df.columns=header
@@ -129,13 +125,13 @@ for mzml in mzmlFiles:
     df['sequence']=df['sequence'].str.replace("\(\w+\)","")
 
     Final_df={}
-    Final_df_header=[’fdr’, 'xcorr’,'deltacn',median_intensity']
+    Final_df_header=['fdr', 'xcorr','deltacn','median_intensity']
     for seq in seqs_new:
-        row=df[df['sequence']==seq].sort_values('score').iloc[[0]]
-        Final_df[seq]=[row['score'].values.tolist()[0],row['MS:1002252'].values.tolist()[0],row['MS:1002253'].values.tolist()[0],row['intensity_cf'].values.tolist()[0]]
+        row=df[df['sequence']==seq].sort('score').iloc[[0]]
+        Final_df[seq]=[row['score'].values.tolist()[0],row['MS:1002252'].values.tolist()[0],row['MS:1002253'].values.tolist()[0],row['intensity'].values.tolist()[0]]
     Final_df=pd.DataFrame(Final_df).transpose()
     Final_df.columns=Final_df_header
-    Final_df.to_csv(file.replace('.csv', '_extracted.csv'))
+    Final_df.to_csv(mergeresult.replace('.featureXML', '_extracted.csv'))
 
 logfile.close()
 subprocess.call(["mv", logfilename, log_path])
